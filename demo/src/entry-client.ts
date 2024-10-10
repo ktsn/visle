6dcdfaaff564e639483f75e471520e0153d051c9
ktsn <ktsn@hey.com>
2024-10-10 22:06:57 +0900

    add inline style to make host node display: contents

--- demo/src/entry-client.ts
+++ demo/src/entry-client.ts
@@ -1,27 +1,32 @@
 import { createSSRApp, defineCustomElement, h, onMounted, useHost } from "vue";
 
-const IslandElement = defineCustomElement({
-	props: {
-		entry: {
-			type: String,
-			required: true,
+const IslandElement = defineCustomElement(
+	{
+		props: {
+			entry: {
+				type: String,
+				required: true,
+			},
+		},
+
+		setup(props) {
+			const host = useHost();
+
+			onMounted(async () => {
+				const entryComponent = (
+					await import(`./components/${props.entry}.client.vue`)
+				).default;
+
+				const app = createSSRApp(entryComponent);
+				app.mount(host!);
+			});
+
+			return () => h("slot");
 		},
 	},
-
-	setup(props) {
-		const host = useHost();
-
-		onMounted(async () => {
-			const entryComponent = (
-				await import(`./components/${props.entry}.client.vue`)
-			).default;
-
-			const app = createSSRApp(entryComponent);
-			app.mount(host!);
-		});
-
-		return () => h("slot");
+	{
+		styles: [":host{display:contents;}"],
 	},
-});
+);
 
 window.customElements.define("vue-island", IslandElement);
