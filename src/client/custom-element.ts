0e27c9909cea0dc31cf6939a41a17f2f0ade241c
ktsn <ktsn@hey.com>
2024-10-18 22:16:52 +0900

    remove unused code

--- src/client/custom-element.ts
+++ src/client/custom-element.ts
@@ -17,30 +17,8 @@ const IslandElement = defineCustomElement(
       const host = useHost()!
 
       onMounted(async () => {
-        const inAnotherIsland = host.parentElement?.closest('vue-island')
-        if (inAnotherIsland) {
-          // Remove island custom element to let the root island handle the hydration.
-          // Since `hydrate` is async function, hydration occurs after all descendants
-          // executed `removeIslandElement`.
-          removeIslandElement()
-        } else {
-          // Hydrate the component if the island is the root.
-          await hydrate()
-        }
+        await hydrate()
       })
-
-      function removeIslandElement(): void {
-        const hostParent = host.parentElement
-        if (!hostParent) {
-          host.remove()
-          return
-        }
-
-        for (const child of host.childNodes) {
-          hostParent.insertBefore(child, host)
-        }
-        host.remove()
-      }
 
       async function hydrate(): Promise<void> {
         const module = await import(/* @vite-ignore */ props.entry)
