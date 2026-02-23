a8611af6fa748f47b9519c3add08c869d4f03208
ktsn <ktsn@hey.com>
2026-02-23 21:45:16 +0900

    fix: suppress warning of nested v-client directive

--- test/fixtures/components/Counter.vue
+++ test/fixtures/components/Counter.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+const { count = 0 } = defineProps<{
+  count?: number
+}>()
+</script>
+
 <template>
-  <button>Count: 0</button>
+  <button>Count: {{ count }}</button>
 </template>
