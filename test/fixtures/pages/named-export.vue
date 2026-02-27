0f68db57a450073f454ea88007a49c59db9426c2
ktsn <ktsn@hey.com>
2026-02-27 20:36:05 +0900

    feat: support named export for wrapped components

---
+++ test/fixtures/pages/named-export.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import NamedExportIsland, { name as islandName } from '../components/NamedExportIsland.vue'
+import NamedExportServer, { name as serverName } from '../components/NamedExportServer.vue'
+</script>
+
+<template>
+  <div>{{ islandName }}</div>
+  <div>{{ serverName }}</div>
+  <NamedExportIsland v-client:load />
+  <NamedExportServer />
+</template>
