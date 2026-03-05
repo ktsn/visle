f2780fa2fe7f39168f78ccc1230ef186258e5412
ktsn <ktsn@hey.com>
2026-03-05 22:56:26 +0900

    test: add named import and barrel file integration tests

---
+++ test/fixtures/pages/with-named-import.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Counter } from '../components/named-import'
+</script>
+
+<template>
+  <div>
+    <h1>Page</h1>
+    <Counter v-client:load />
+  </div>
+</template>
