b9888bef10ee0198ff0dd12f6f091e6f8fc5ff20
ktsn <ktsn@hey.com>
2026-03-09 20:42:17 +0900

    docs: set Manrope font for docs headings

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,6 +6,28 @@ export default defineConfig({
 
   head: [
     [
+      'link',
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.googleapis.com',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+        crossorigin: '',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Manrope:wght@600;700;800&display=swap',
+      },
+    ],
+    [
       'style',
       {},
       `:root {
@@ -19,6 +41,10 @@ export default defineConfig({
   --vp-c-brand-2: #34c07d;
   --vp-c-brand-3: #2baa6e;
   --vp-c-brand-soft: rgba(61, 214, 140, 0.16);
+}
+.vp-doc h1, .vp-doc h2, .vp-doc h3, .vp-doc h4,
+.VPHero .name, .VPHero .text {
+  font-family: 'Manrope', sans-serif;
 }`,
     ],
   ],
