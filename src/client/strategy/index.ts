1659e5687ca12383920786900ff9df335c071e50
ktsn <ktsn@hey.com>
2026-03-16 22:50:40 +0900

    chore: fix typo

--- src/client/strategy/index.ts
+++ src/client/strategy/index.ts
@@ -1,6 +1,6 @@
 import { IslandStrategy } from '../custom-element.js'
 import { load } from './load.js'
-import { visible } from './visble.js'
+import { visible } from './visible.js'
 
 export const strategies: Record<string, IslandStrategy> = {
   load,
